5ddc12a72b8d04c97a4d908aedeb4f055f9f8ab5
Milind Jain <jainmilind10112002@gmail.com>
2022-04-25 14:38:15 +0530

    Add answer model

---
+++ answer/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from question.models import Question
+from users.models import User
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=1000)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    class Meta:
+        get_latest_by = "timestamp"
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return self.text
+
+
+class AnswerComment(models.Model):
+    text = models.CharField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    answer = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        get_latest_by = "timestamp"
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return str(self.text)
