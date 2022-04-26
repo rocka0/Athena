8695b4dd8ebe6dc2b663b716715b1a1bec146ff2
Tushar <tusky2002@gmail.com>
2022-04-26 16:15:38 +0530

    Added frontend for getQuestionById

--- question/views.py
+++ question/views.py
@@ -1,19 +1,22 @@
-from django.db import connection
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.http import HttpResponseNotFound
+from django.shortcuts import redirect, render
 from .models import *
 from answer.models import Answer, AnswerComment
+from users.views import isUserLoggedIn
 
 
 def show_question(response, id):
+    if not isUserLoggedIn(response):
+        return redirect('userLogin')
+
     question_obj = Question.objects.raw(
         f"SELECT title, text, id FROM question_question WHERE id={id}"
     )
-    
+
     if len(question_obj) == 0:
         return HttpResponseNotFound('404: Question does not exist')
-    
-    question_dict = {
+
+    context = {
         "title": question_obj[0].title,
         "text": question_obj[0].text,
         "question_comments": [],
@@ -27,11 +30,11 @@ def show_question(response, id):
         comments = AnswerComment.objects.raw(
             f"SELECT text, id, timestamp FROM answer_answercomment WHERE answer_id={answer.id}"
         )
-        question_dict["answers"].append(
+        context["answers"].append(
             {"answer": answer, "comments": comments})
 
-    question_dict["question_comment"] = QuestionComment.objects.raw(
+    context["question_comment"] = QuestionComment.objects.raw(
         f'SELECT text, id, timestamp FROM question_questioncomment WHERE question_id={id}'
     )
-    return render(response, "question/index.html", question_dict)
-
+    context["userLoggedIn"] = True
+    return render(response, "question/index.html", context)
