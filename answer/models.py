5b482e5811931efa7200b9656bdffe2ae763ca6a
Milind Jain <jainmilind10112002@gmail.com>
2022-04-26 18:47:37 +0530

    Fixed answer comments model

--- answer/models.py
+++ answer/models.py
@@ -20,7 +20,7 @@ class Answer(models.Model):
 class AnswerComment(models.Model):
     text = models.CharField(max_length=500)
     timestamp = models.DateTimeField(auto_now_add=True)
-    answer = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
